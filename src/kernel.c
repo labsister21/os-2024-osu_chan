ce0d9a8270f513da720c80144925d536d48999f0
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-02-28 13:55:38 +0700

    edit gitignore

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "header/cpu/gdt.h"
+// #include "gdt.c"
 #include "header/kernel-entrypoint.h"
 
+// void kernel_setup(void) {
+//     uint32_t a;
+//     uint32_t volatile b = 0x0000BABE;
+//     __asm__("mov $0xCAFE0000, %0" : "=r"(a));
+//     // load_gdt(&_gdt_gdtr);
+//     while (true) b += 1;
+// }
 void kernel_setup(void) {
-    uint32_t a;
-    uint32_t volatile b = 0x0000BABE;
-    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
     load_gdt(&_gdt_gdtr);
-    while (true) b += 1;
+    while (true);
 }
