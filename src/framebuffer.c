f3a63920f105ab54f6f1f47392120d88c0b09b83
HanifIHM <imamhanif2003@gmail.com>
2024-03-29 11:45:52 +0700

    framebuffer

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -32,15 +32,30 @@ uint16_t get_cursor_position(void)
 
 void framebuffer_set_cursor(uint8_t r, uint8_t c)
 {
-    // TODO : Implement
+    uint16_t pos = r * 80 + c;
+ 
+	out(CURSOR_PORT_CMD, 0x0F);
+	out(CURSOR_PORT_DATA, (uint8_t) (pos & 0xFF));
+	out(CURSOR_PORT_CMD, 0x0E);
+	out(CURSOR_PORT_DATA, (uint8_t) ((pos >> 8) & 0xFF));  
 }
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 {
-    // TODO : Implement
+    uint16_t attrib = (bg << 4) | (fg & 0x0F);
+    volatile uint16_t * where;
+    where = (volatile uint16_t *)0xB8000 + (row * 80 + col) ;
+    *where = c | (attrib << 8);
 }
 
 void framebuffer_clear(void)
 {
-    // TODO : Implement -
+    for (int i = 0; i < 25; i++)
+    {
+        for (int j = 0; j < 80; j++)
+        {
+            framebuffer_write(i,j, 0x00, 0x07, 0x00);
+        }
+    }
+    
 }
--- src/kernel.c
+++ src/kernel.c
@@ -3,12 +3,24 @@
 #include "header/cpu/gdt.h"
 // #include "gdt.c"
 #include "header/kernel-entrypoint.h"
+#include "header/text/framebuffer.h"
 
 void kernel_setup(void) {
     // uint32_t a;
     // uint32_t volatile b = 0x0000BABE;
     // __asm__("mov $0xCAFE0000, %0" : "=r"(a));
     load_gdt(&_gdt_gdtr);
+    // framebuffer_clear();
+    // framebuffer_write(3, 8,  'H', 0, 0xF);
+    // framebuffer_write(3, 9,  'a', 0, 0xF);
+    // framebuffer_write(3, 10, 'i', 0, 0xF);
+    // framebuffer_write(3, 11, '!', 0, 0xF);
+    // framebuffer_set_cursor(3, 10);
+    // framebuffer_write(4, 8,  'H', 0, 0xF);
+    // framebuffer_write(4, 9,  'a', 0, 0xF);
+    // framebuffer_write(4, 10, 'i', 0, 0xF);
+    // framebuffer_write(4, 11, '!', 0, 0xF);
+    // framebuffer_set_cursor(4, 10);
     while(true);
     // while (true) b += 1;
 }
