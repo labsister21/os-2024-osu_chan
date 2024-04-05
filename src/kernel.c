d7e827bebc89a4ffb7d3b808018909898f3c4a03
Francesco <francescokusuma15@gmail.com>
2024-04-05 18:05:14 +0700

    fdt

--- src/kernel.c
+++ src/kernel.c
@@ -51,34 +51,34 @@ void kernel_setup(void)
     //      get_keyboard_buffer(&c);
     //      if (c) framebuffer_write(0, col++, c, 0xF, 0);
     // }
-    // load_gdt(&_gdt_gdtr);
-    // pic_remap();
-    // initialize_idt();
-    // activate_keyboard_interrupt();
-    // framebuffer_clear();
-    // framebuffer_set_cursor(0, 0);
-
-    // // int col = 0;
-    // while (true)
-    // {
-    //     //  char c;
-    //     //  get_keyboard_buffer(&c);
-    //     //  if (c) framebuffer_write(0, col++, c, 0xF, 0);
-    //     keyboard_state_activate();
-    // }
     load_gdt(&_gdt_gdtr);
     pic_remap();
-    activate_keyboard_interrupt();
     initialize_idt();
+    activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
 
-    struct BlockBuffer b;
-    for (int i = 0; i < 512; i++){
-        b.buf[i] = i % 16;
+    // int col = 0;
+    while (true)
+    {
+        //  char c;
+        //  get_keyboard_buffer(&c);
+        //  if (c) framebuffer_write(0, col++, c, 0xF, 0);
+        keyboard_state_activate();
     }
-    write_blocks(&b, 17, 1);
-    while (true);
+    // load_gdt(&_gdt_gdtr);
+    // pic_remap();
+    // activate_keyboard_interrupt();
+    // initialize_idt();
+    // framebuffer_clear();
+    // framebuffer_set_cursor(0, 0);
+
+    // struct BlockBuffer b;
+    // for (int i = 0; i < 512; i++){
+    //     b.buf[i] = i % 16;
+    // }
+    // write_blocks(&b, 17, 1);
+    // while (true);
 }
 // void kernel_setup(void)
 // {
